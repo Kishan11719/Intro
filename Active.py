49378993fbe1086172c57de7f90049b475bf8a83
Kishan11719 <narendragaddamiitmaths@gmail.com>
2024-10-05 18:08:18 +0530

    pyhton

---
+++ Active.py
@@ -0,0 +1,11 @@
+# printing a greeting message 
+print("Hello, world!")
+
+# printing my name
+print("My name is Kishan")
+
+# printing a fun fact about myself
+print("I love playing basketball")
+
+# performing and printing a simple calculation
+print("The result of 3+5 is:", 3+5)
---
+++ Active2.py
@@ -0,0 +1,13 @@
+#print a sentence
+print("Welcome to the world of programing")
+
+#print a Number
+print(4)
+
+#Next line after print
+print("Hello pyton \n")
+
+#print all the keywords present in pyton
+print("Keywords in python are....")
+import keyword
+print(keyword.kwlist)
